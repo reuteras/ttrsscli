5810089a0c7df68cfd994a2f14508d92b7c1ffd2
Peter Reuterås <peter@reuteras.com>
2025-03-03 14:29:48 +0100

    Use init from super

--- src/ttrsscli/ui/widgets.py
+++ src/ttrsscli/ui/widgets.py
@@ -22,6 +22,15 @@ ALLOW_IN_FULL_SCREEN: list[str] = [
 class LinkableMarkdownViewer(MarkdownViewer):
     """An extended MarkdownViewer that allows web links to be clicked."""
 
+    def __init__(self, **kwargs) -> None:
+        """Initialize the LinkableMarkdownViewer.
+
+        Args:
+            **kwargs: Additional arguments
+        """
+        super().__init__(**kwargs)
+
+
     @on(message_type=Markdown.LinkClicked)
     def handle_link(self, event: Markdown.LinkClicked) -> None:
         """Open links in the default web browser.
